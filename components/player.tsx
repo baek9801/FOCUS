467ee079482b6d531ee04c4180f96fe350b0e3b7
baek9801 <127748482+baek9801@users.noreply.github.com>
2023-05-04 15:41:25 +0900

    fixed error

--- components/player.tsx
+++ components/player.tsx
@@ -44,9 +44,7 @@ export default function SpotifyPlayer() {
     }
     async function getPlaylist() {
       try {
-        const seedGenres = userInfo.genres.length
-          ? userInfo.genres.join(",")
-          : "pop";
+        const seedGenres = userInfo.genres ? userInfo.genres.join(",") : "pop";
         const searchResponse = await axios.get(
           `https://api.spotify.com/v1/recommendations?market=US&seed_genres=${seedGenres}&limit=6`,
           {
